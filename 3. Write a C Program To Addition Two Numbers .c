b3aff4dda9ce9ae7d43a41d1163f1e042f5021ff
Pintu Hembram <90277086+PintuHembram@users.noreply.github.com>
2024-08-13 15:53:20 +0530

    Update 3. Write a C Program To Addition Two Numbers .c

--- 3. Write a C Program To Addition Two Numbers .c
+++ 3. Write a C Program To Addition Two Numbers .c
@@ -1,6 +1,13 @@
-#include<stdio.h>
-#includ<conio.h>
-void main()
-{
-  int a, b, sum;
-clrscr(); //This faction
+	#include <stdio.h>
+	#include <conio.h> //This faction only used Turbu++
+	void main() 
+	{
+	    int a, b, sum;
+	    clrscr(); //This Faction Only Used To Turbu++
+	    printf("enter the value of a\n");
+	    scanf("%d", &a);
+	    printf("enter the value of b\n");
+	    scanf("%d", &b);
+	    sum = a + b;
+	    printf("The sum is %d", sum);
+	}
