a9c6bea5a6fd8428a082b99f9472c32cf2291e3b
Pintu Hembram <90277086+PintuHembram@users.noreply.github.com>
2024-08-13 11:35:48 +0530

    Create 2. Write a C Program to Subtraction two Numbers .c

---
+++ 2. Write a C Program to Subtraction two Numbers .c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a, b, sub;clrscr(); // This faction only Used to Turbu++.
+printf("Enter the value of a\n");
+scanf("%d", &b;
+sub = a - b;
+printf("The Subtraction Result is %d", sub);
+}
